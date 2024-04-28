d478bd09dfbbb705acc9c6ff50807382a71915e0
tj567 <89848037+tj567@users.noreply.github.com>
2024-04-28 11:12:07 +0100

    Add files via upload

---
+++ proj104/proj104.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread("solar-system.jpg")
+cv2.putText(img,"sun",(10,250),cv2.FONT_HERSHEY_COMPLEX,1.5,(255,255,255))
+cv2.putText(img,"mercury",(105,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"mars",(200,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"earth",(300,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"venus",(400,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"jupiter",(500,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"saturn",(800,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"uranus",(950,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"neptune",(1100,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.imshow("output",img)
+cv2.waitKey(0)
+cv2.imwrite("solar system with name",img)
